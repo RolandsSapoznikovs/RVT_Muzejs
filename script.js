86a0cb260d262609dc50d957565392a78d7c6b98
RolandsSapoznikovs <rolands_sapoznikovs@inbox.lv>
2024-01-20 19:52:57 +0200

    Adding the images and popup buttons

---
+++ script.js
@@ -0,0 +1,29 @@
+function BrivaisLaiksBack()
+{
+    location.href = "Audzeknu_ikdiena.html";
+}
+
+function BrivaisLaiksFroward()
+{
+    location.href = "Brivais_laiks_izvelne.html";
+}
+
+function DejasBack()
+{
+    location.href = "Brivais_laiks_izvelne.html";
+}
+
+function DejasForward()
+{
+    location.href = "Dejas2.html";
+}
+
+function Dejas2Back()
+{
+    location.href = "Dejas.html";
+}
+
+function Dejas2Forward()
+{
+    location.href = "Dejas2.html";
+}
