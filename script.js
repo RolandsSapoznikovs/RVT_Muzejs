8867fcbc01d2532d42823a867c10d119a276c0bd
RolandsSapoznikovs <rolands_sapoznikovs@inbox.lv>
2024-01-27 18:39:50 +0200

    Fixing the dejas subsection, adding the sports subsection, adding the vesture section and pirmscesture subsection, also adding some pictures from the arshive file

--- script.js
+++ script.js
@@ -28,6 +28,46 @@ function Dejas2Forward()
     location.href = "Dejas2.html";
 }
 
+function SportsBack()
+{
+    location.href = "Audzeknu_ikdiena.html";
+}
+
+function SportsForward()
+{
+    location.href = "Sports2.html"
+}
+
+function Sports2Back()
+{
+    location.href = "Sports.html";
+}
+
+function Sports2Forward()
+{
+    location.href = "Sports3.html"
+}
+
+function Sports3Back()
+{
+    location.href = "Sports2.html";
+}
+
+function Sports3Forward()
+{
+    location.href = "Sports3.html"
+}
+
+function PirmsvestureBack()
+{
+    location.href = "Vesture.html"
+}
+
+function PirmsvestureForward()
+{
+    location.href = "Pirmsvesture_izvelne.html"
+}
+
 const openBtn = document.getElementById("DejasMore");
 const closeBtn = document.getElementById("CloseDejasPopup");
 const modal = document.getElementById("modal");
